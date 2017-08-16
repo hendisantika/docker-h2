31a5f68be403f05cb97e8e57145d488b0fe737ab
Hendi Santika <hendisantika@gmail.com>
2017-08-16 07:32:09 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/dockerh2/DockerH2Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.dockerh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerH2Application.class, args);
+	}
+}
