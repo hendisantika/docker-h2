82791a0d227023b95e0ab5160fe08018934866a5
Hendi Santika <hendisantika@gmail.com>
2017-08-16 07:40:27 +0700

    #6 - Added Dockerfile

--- src/main/java/com/hendisantika/dockerh2/UserController.java
+++ src/main/java/com/hendisantika/dockerh2/UserController.java
@@ -1,5 +1,6 @@
 package com.hendisantika.dockerh2;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
