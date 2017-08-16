5e49287e3bce5235d12c828ac487d00a73646e66
Hendi Santika <hendisantika@gmail.com>
2017-08-16 07:36:13 +0700

    #4 - Added User Repository

---
+++ src/main/java/com/hendisantika/dockerh2/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.dockerh2;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/16/17
+ * Time: 7:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+}
